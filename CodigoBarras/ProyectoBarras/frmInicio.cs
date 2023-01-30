8d682b3e5f60fad7cfc4435fe281c33dda559554
Adolfo Scheglel <adolfo.77@outlook.es>
2023-01-30 06:49:23 -0300

    23.01.30 06.50

---
+++ CodigoBarras/ProyectoBarras/frmInicio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoBarras
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Closing(object sender, FormClosingEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void btngenerarmanualmente_Click(object sender, EventArgs e)
+        {
+            Form1 FormularioVista = new Form1();
+            this.Hide();
+            FormularioVista.Show();
+            FormularioVista.FormClosing += Frm_Closing;
+        }
+
+        private void btngenerarmasivamente_Click(object sender, EventArgs e)
+        {
+            frmMasivo FormularioVista = new frmMasivo();
+            this.Hide();
+            FormularioVista.Show();
+            FormularioVista.FormClosing += Frm_Closing;
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
